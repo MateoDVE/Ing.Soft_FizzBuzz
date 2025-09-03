d541f40f3d395aff636b6f116da414e9eb898dc0
Mateo_DVE <matu2207@gmail.com>
2025-09-02 23:36:22 -0400

    multiplo de 3 Fizz

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -3,8 +3,15 @@ function EsValor1(n) {
   return n === 1;
 }
 
+function EsMultiploDe3(n) {
+  return n % 3 === 0;
+}
+
 export function ConvertirFizzBuzz(n) {
   if(EsValor1(n)) {
     return '1';
+  }
+  if (EsMultiploDe3(n)) {
+    return 'Fizz';
   }
 }
--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -4,4 +4,8 @@ describe('Ejemplo de FizzBuzz', () => {
   it('debería retornar 1', () => {
     expect(ConvertirFizzBuzz(1)).toEqual('1');
   });
+
+  it('debería retornar Fizz', () => {
+    expect(ConvertirFizzBuzz(3)).toEqual('Fizz');
+  });
 });
