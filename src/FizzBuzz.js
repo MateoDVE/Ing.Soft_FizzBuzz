ba6b4c34c808f975279a63ce206c0eddffc51c2f
Mateo_DVE <matu2207@gmail.com>
2025-09-02 23:48:31 -0400

    fizzbuzz cuando retorna 3 y 5

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -10,22 +10,20 @@ function EsMultiploDe3(n) {
 function EsMultiploDe5(n) {
   return n % 5 === 0;
 }
+
 function EsMultiploDe3y5(n) {
   return EsMultiploDe3(n) && EsMultiploDe5(n);
 }
 
 export function ConvertirFizzBuzz(n) {
-  if(EsValor1(n)) {
-    return '1';
+  if (EsMultiploDe3y5(n)) {
+    return 'FizzBuzz';
   }
   if (EsMultiploDe3(n)) {
     return 'Fizz';
   }
   if (EsMultiploDe5(n)) {
     return 'Buzz';
-  }
-  if (EsMultiploDe3y5(n)) {
-    return 'FizzBuzz';
   }
   return `${n}`;
 }
--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -16,4 +16,8 @@ describe('Ejemplo de FizzBuzz', () => {
     it('debería retornar Buzz si es igual a 5', () => {
     expect(ConvertirFizzBuzz(5)).toEqual('Buzz');
   });
+  
+  it('debería retornar FizzBuzz si es múltiplo de 3 y de 5', () => {
+    expect(ConvertirFizzBuzz(15)).toEqual('FizzBuzz');
+  });
 });
