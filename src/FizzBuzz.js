42bf98cfa3b8ab0ed3cc3f033940fd020a550064
Mateo_DVE <matu2207@gmail.com>
2025-09-02 23:31:52 -0400

    retorno de uno

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,15 +1,10 @@
-function EsMultiploDe3(n) {
-  return n % 3 === 0;
-}
+
 function EsValor1(n) {
   return n === 1;
 }
 
 export function ConvertirFizzBuzz(n) {
   if(EsValor1(n)) {
-    return '1';
-  }
-  if (EsMultiploDe3(n)) {
     return '1';
   }
 }
